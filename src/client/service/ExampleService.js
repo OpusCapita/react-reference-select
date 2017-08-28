7c6aac9ff8ddb8b9c0f529377c56c717adb8259f
Dmitriy Sanko <dsanko@scand.com>
2017-08-28 13:06:43 +0300

    (CMMN-5361) Removed example REST API. Refactoring code.

--- src/client/service/ExampleService.js
+++ src/client/service/ExampleService.js
@@ -1,17 +1,55 @@
-import request from "superagent-bluebird-promise";
-
 export default class ExampleService {
-  constructor(serviceUrl) {
-    this._exampleServiceUrl = `${serviceUrl}/api/examples`;
-  }
+
+  examples = {
+    "body": [
+      {
+        "id": "1",
+        "name": "example1",
+        "_objectLabel": "example1"
+      },
+      {
+        "id": "2",
+        "name": "example2",
+        "_objectLabel": "example2"
+      },
+      {
+        "id": "3",
+        "name": "example3",
+        "_objectLabel": "example3"
+      }
+    ],
+    "headers": {
+      "content-range": "items 0-2/3"
+    }
+  };
+
 
   getExamples(params) {
-    return request.get(this._exampleServiceUrl).query(params).set('Accept', 'application/json');
-  }
-
-  getExample(id) {
-    return request.
-    get(`${this._exampleServiceUrl}/${encodeURIComponent(id)}`).
-    set('Accept', 'application/json');
+    if (params.id || params.name) {
+      let items = [];
+      if (params.id) {
+        items = this.examples.body.filter((example) => {
+          return example.id.includes(params.id)
+        })
+      }
+      if (params.name) {
+        if (items.length) {
+          items = items.filter((example) => {
+            return example.name.includes(params.name)
+          });
+        } else {
+          items = this.examples.body.filter((example) => {
+            return example.name.includes(params.name)
+          });
+        }
+      }
+      return Promise.resolve({
+        "body": items,
+        "headers": {
+          "content-range": `items 0-9/${items.length}`
+        }
+      })
+    }
+    return Promise.resolve(this.examples)
   }
 }
