60849eb7a495b78bb4beb30131d6aeb68441ee13
Dmitriy Sanko <dsanko@scand.com>
2017-08-29 17:36:41 +0300

    (CMMN-5361) Fixing components demo page generation.

--- scripts/gh-pages/generate.sh
+++ scripts/gh-pages/generate.sh
@@ -3,8 +3,10 @@
 rm -rf .gh-pages-tmp lib demo.tar.gz  &&
 mkdir .gh-pages-tmp &&
 
-node node_modules/webpack/bin/webpack.js --config ./webpack.production.config.js --hide-modules &&
-cp -R build/* .gh-pages-tmp &&
+node node_modules/webpack/bin/webpack.js --config ./webpack.docs.config.js --hide-modules &&
+mkdir .gh-pages-tmp/static .gh-pages-tmp/css &&
+cp -R static/* .gh-pages-tmp/static &&
+cp -R src/client/demo/css/* .gh-pages-tmp/css &&
 cp -R src/client/demo/index.html .gh-pages-tmp &&
 cp -R .gitignore .gh-pages-tmp &&
 tar -cvzf demo.tar.gz .gh-pages-tmp
