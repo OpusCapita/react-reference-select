45f1885fddc820edfeeabaab3dceada772aac288
Andrey Shchitlyak <shchitlyak@scand.com>
2019-01-09 09:22:58 +0300

    Adjust implementation

--- src/client/components/ReactSelectSpecificProps/index.js
+++ src/client/components/ReactSelectSpecificProps/index.js
@@ -1,7 +1,11 @@
 import PropTypes from 'prop-types';
 
 export default {
-  defaultOptions: PropTypes.bool,
+  /**
+  * The props 'key' was added for resolving issue (https://github.com/JedWatson/react-select/issues/1581).
+  * Need to find a better solution in the future.
+  */
+  key: PropTypes.string,
   clearable: PropTypes.bool,
   onOpen: PropTypes.func,
   className: PropTypes.string,
