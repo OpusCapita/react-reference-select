64668dd0daa7d6693bf603ccb7a3fefca15c40b2
Andrey Shchitlyak <shchitlyak@scand.com>
2019-01-09 09:27:23 +0300

    Replaced comment

--- src/client/components/ReactSelectSpecificProps/index.js
+++ src/client/components/ReactSelectSpecificProps/index.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 
+/**
+ * The props 'key' was added for resolving issue (https://github.com/JedWatson/react-select/issues/1581).
+ * Need to find a better solution in the future.
+*/
+
 export default {
-  /**
-  * The props 'key' was added for resolving issue (https://github.com/JedWatson/react-select/issues/1581).
-  * Need to find a better solution in the future.
-  */
   key: PropTypes.string,
   clearable: PropTypes.bool,
   onOpen: PropTypes.func,
