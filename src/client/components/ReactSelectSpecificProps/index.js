27e60e3f1328afc1c66392e77d85039a81e122f0
Andrey Shchitlyak <shchitlyak@scand.com>
2019-02-25 18:02:54 +0300

    Added cacheOptions prop for caching load options

--- src/client/components/ReactSelectSpecificProps/index.js
+++ src/client/components/ReactSelectSpecificProps/index.js
@@ -6,6 +6,11 @@ export default {
   * Need to find a better solution in the future.
   */
   key: PropTypes.string,
+  /**
+  * If cacheOptions is truthy, then the loaded data will be cached. The cache
+  * will remain until `cacheOptions` changes value.
+  */
+  cacheOptions: PropTypes.bool,
   clearable: PropTypes.bool,
   onOpen: PropTypes.func,
   className: PropTypes.string,
